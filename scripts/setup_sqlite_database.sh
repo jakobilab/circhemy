29b025681aa1bf0695a94e6ea8b6d5ecddef0789
tjakobi <tobias.jakobi@med.uni-heidelberg.de>
2023-01-12 17:22:52 -0700

    Adding Bash-based SQLite3 database setup script

---
+++ scripts/setup_sqlite_database.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Copyright (C) 2023 Tobias Jakobi
+#
+# @Author: Tobias Jakobi <tjakobi>
+# @Email:  tjakobi@arizona.edu
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if ! command -v sqlite3 &> /dev/null
+then
+    echo "SQLite3 is required, but not installed or in \$PATH."
+    (exit 1)
+fi
+
+
+echo "Loading base SQL table schema...old database will be ERASED!"
+
+sqlite3 ../circconvert.sqlite3 < ../data/circrnadb_schema.sql
+
+echo "SQL database schema ready, starting data import"
+
+sqlite3 ../circconvert.sqlite3 << EOF
+.mode csv
+.import ../data/raw/sqlite_export.csv circrnadb
+VACUUM;
+EOF
+
+echo "Data import finished."
