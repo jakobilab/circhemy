b80caa1c04785063d99e519fe60c5222f90f0837
tjakobi <tobias.jakobi@med.uni-heidelberg.de>
2024-04-10 15:34:59 -0700

    Copyright update

--- circhemy/circhemy_web.py
+++ circhemy/circhemy_web.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Tobias Jakobi
+# Copyright (C) 2024 Tobias Jakobi
 #
 # @Author: Tobias Jakobi <tjakobi>
 # @Email:  tjakobi@arizona.edu
--- circhemy/cli/cli.py
+++ circhemy/cli/cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2023 Tobias Jakobi
+# Copyright (C) 2024 Tobias Jakobi
 #
 # @Author: Tobias Jakobi <tjakobi>
 # @Email:  tjakobi@arizona.edu
